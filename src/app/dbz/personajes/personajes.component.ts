fba4ad808cb0164c40fb959185d30dfc9801a4ef
JuanTrejo <carlostrejo771@gmail.com>
2022-05-27 17:48:45 -0500

    Second Version: Input

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent {
+
+  @Input() personajes: Personaje[] = [{
+    nombre: 'Goku',
+    poder: 10000
+  }]
+
+}
